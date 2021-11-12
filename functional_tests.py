1574f1ce019696d0fe94e3f72446c2466a318222
VerbGardner <brokenmind@gmail.com>
2021-11-12 01:21:02 -0600

    Updated functional_tests.py to use unittest module and more cleanly run tests.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,26 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert "successfully" in browser.title
+    def tearDown(self):
+        #They get frustrated and exit quit out of the entire browser angry.
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #David heard about a cool new online recipe site. They go to check out its  homepage.
+        self.browser.get('http://localhost:8000')
+
+
+        #They notice the page title and header mention it's a cooking project.
+        self.assertIn("Cooking Project",self.browser.title)
+        self.fail("Finish the test!")
+
+        self.assertIn('successfully',self.browser.title)
+        self.fail("Keep going...")
+
+
+if __name__ == '__main__':
+    unittest.main(warnings="ignore")
