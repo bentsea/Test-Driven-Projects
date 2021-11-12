9082dcc69e9c2d83e6782cdcf7c99c92b6899b58
VerbGardner <brokenmind@gmail.com>
2021-11-12 12:37:48 -0600

    Moved secret key to .env and changed it from the default generated one because it's just so much easier than trying to scrub a key out of  key out of github.

--- superlists/settings.py
+++ superlists/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+#load environment variables.
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d-0a4f+&!jgbp3o705a4raw+$h$4o3%@h1hb3)aqm4!%$b0db$'
+SECRET_KEY = os.environ.get('SECRET_KEY',None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
