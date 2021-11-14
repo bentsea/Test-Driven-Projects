ba6addc19bfe8f053779619732ae9d5bba036ef1
VerbGardner <brokenmind@gmail.com>
2021-11-14 16:17:52 -0600

    Basic view now returns minimal HTML.

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render, HttpResponse
 
 def home_page(request):
-    return HttpResponse("<h1>Hello World!</h1>")
+    return HttpResponse("<html><body><title>Untitled Cooking Project</title><h1>Hello World!</h1></body></html>")
