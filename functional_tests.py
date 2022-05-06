83bb4343d182242ff53fd454405a0b43667c3122
VerbGardner <brokenmind@gmail.com>
2022-05-06 00:12:48 -0500

    Functional test now checks that you can input an ingredient.

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,7 +18,34 @@ class NewVisitorTest(unittest.TestCase):
 
         #They notice the page title and header mention it's a cooking project.
         self.assertIn("Cooking Project",self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn("Cooking Project",header_text)
+
+        #They are immediately prompted to create a recipe.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a recipe ingredient'
+        )
+
+        #They type in 1lb chopped human liver into a text box (their hobby is cannibalism)
+        inputbox.send_keys('1Lb chopped human liver')
+
+        #When they hit enter, the page updates , and now the page lists "1. 1Lb chopped human liver" as an ingredient in a recipe.
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1. 1Lb chopped human liver' for row in rows)
+        )
+
+        #There is still a text box inviting the user to add another item. They enter 'Minced finger tips'
         self.fail("Finish the test!")
+
+        #The page updates again, and now both ingredients appear on the list.
+        [...]
 
 
 
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render, HttpResponse
 
 def home_page(request):
-    return HttpResponse("<html><body><title>Untitled Cooking Project</title><h1>Hello World!</h1></body></html>")
+    return HttpResponse("<html><body><title>Untitled Cooking Project</title><h1>Untitled Cooking Project</h1></body></html>")
