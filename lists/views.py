5383e6520842cfaf1ca4bf1da2dc5c3747ff4aba
VerbGardner <brokenmind@gmail.com>
2022-05-06 01:57:56 -0500

    Refactored home url and created template registered lists with app Updated tests to be more efficient

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 def home_page(request):
-    return HttpResponse("<html><body><title>Untitled Cooking Project</title><h1>Untitled Cooking Project</h1></body></html>")
+    return render(request,'home.html')
