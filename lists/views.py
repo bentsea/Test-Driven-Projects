2e9b487842ad97b6d2b9e1d556a2d2ab899865a5
VerbGardner <brokenmind@gmail.com>
2022-05-23 01:21:05 -0500

    Refactored

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from lists.models import Item
 
 def home_page(request):
-    return render(request,'home.html', {'new_item_text':request.POST.get('item_text','')})
+    post_item_text = request.POST.get('item_text','')
+    if post_item_text != '':
+        item = Item()
+        item.text = post_item_text
+        item.save()
+    return render(request,'home.html', {'new_item_text':post_item_text})
